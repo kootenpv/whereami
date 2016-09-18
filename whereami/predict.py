c0f52a1d6ce986a00b7bd14a2c91976719f00c2e
Pascal van Kooten <kootenpv@gmail.com>
2016-09-18 19:57:29 +0200

    cache model later

--- whereami/predict.py
+++ whereami/predict.py
@@ -3,13 +3,13 @@ from whereami.pipeline import LocationPipeline
 from whereami.get_data import get_train_data
 from whereami.get_data import sample
 
-X, y = get_train_data()
-
-lp = LocationPipeline()
-lp.fit(X, y)
-
 
 def predict(tts=False):
+    X, y = get_train_data()
+
+    lp = LocationPipeline()
+    lp.fit(X, y)
+
     if tts:
         pred = lp.predict(sample())[0]
         os.system("say " + pred + " &")
