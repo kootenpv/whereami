055f7edaad135cdfacd5fdd437eb0d53e887a40e
Bruno <brunocauet+github@gmail.com>
2016-09-27 17:53:54 +0100

    Update get_data.py
    - Rely on ensure_whereami_path() in get_train_data()
    - Don't construct & merge (append, extend, +) unneeded lists
    - Rely on dict comprehension syntax for a shorter sample()

--- whereami/get_data.py
+++ whereami/get_data.py
@@ -3,29 +3,23 @@ import subprocess
 import os
 from access_points import get_scanner
 
+from whereami.utils import ensure_whereami_path
+
 
 def sample():
     wifi_scanner = get_scanner()
     aps = wifi_scanner.get_access_points()
-    dc = {}
-    for ap in aps:
-        key = ap['ssid'] + " " + ap['bssid']
-        value = ap['quality']
-        dc[key] = value
-    return dc
-
+    return {ap['ssid'] + " " + ap['bssid']: ap['quality'] for ap in aps}
 
 def get_train_data(folder=None):
     if folder is None:
-        folder = os.path.expanduser("~/.whereami")
+        folder = ensure_whereami_path()
     X = []
     y = []
     for fname in os.listdir(folder):
         if fname.endswith(".txt"):
-            data = []
             with open(os.path.join(folder, fname)) as f:
-                for line in f:
-                    data.append(json.loads(line))
+                data = map(json.loads, f)
             X.extend(data)
-            y.extend([fname.rstrip(".txt")] * len(data))
+            y.extend(fname.rstrip(".txt") for _ in data)
     return X, y
