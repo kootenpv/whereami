239113976805fa193b47034bafc941bdfeb94bf1
Pascal van Kooten <kootenpv@gmail.com>
2016-09-27 22:24:49 +0200

    using argparse to provide a better cli experience

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 MAJOR_VERSION = '0'
 MINOR_VERSION = '1'
-MICRO_VERSION = '17'
+MICRO_VERSION = '18'
 VERSION = "{}.{}.{}".format(MAJOR_VERSION, MINOR_VERSION, MICRO_VERSION)
 
 setup(name='whereami',
--- whereami/__main__.py
+++ whereami/__main__.py
@@ -1,18 +1,32 @@
-import sys
 from whereami.predict import predict
 from whereami.predict import predict_proba
 from whereami.predict import crossval
 from whereami.learn import learn
 
 
+def get_args_parser():
+    import argparse
+    desc = 'Uses WiFi signals and machine learning to predict where you are.'
+    p = argparse.ArgumentParser(description=desc)
+    subparsers = p.add_subparsers(dest="command")
+    subparsers.add_parser('predict')
+    subparsers.add_parser('predict_proba')
+    subparsers.add_parser('crossval')
+    learn_parser = subparsers.add_parser('learn')
+    learn_parser.add_argument('--location', '-l', required=True,
+                              help='A name-tag for location to learn.')
+    learn_parser.add_argument('--num_samples', '-n', type=int, default=100,
+                              help='Number of samples to take')
+    return p
+
+
 def main():
-    if "predict_proba" == sys.argv[1]:
+    args = get_args_parser().parse_args()
+    if args.command == "predict_proba":
         predict_proba()
-    elif "predict" == sys.argv[1]:
+    elif args.command == "predict":
         predict()
-    elif "learn" in sys.argv[1] and len(sys.argv) == 4:
-        category = sys.argv[2]
-        n = int(sys.argv[3])
-        learn(category, n)
-    elif "crossval" in sys.argv[1]:
+    elif args.command == "learn":
+        learn(args.location, args.num_samples)
+    elif args.command == "crossval":
         crossval()
