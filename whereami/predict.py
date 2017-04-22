6842ae4223a61d762b3e55d47ef73a62a2cc9fb2
Andrew Dessin (Spartain) <andrew.dessin@gmail.com>
2017-04-21 22:19:27 -0400

    Added external data support

--- whereami/predict.py
+++ whereami/predict.py
@@ -1,25 +1,29 @@
 from collections import Counter
 
-from whereami.get_data import get_train_data
+from whereami.compat import cross_val_score
+from whereami.get_data import get_train_data, get_external_sample
 from whereami.get_data import sample
 from whereami.pipeline import get_model
-from whereami.compat import cross_val_score
 
 
-def predict_proba():
-    lp = get_model()
-    print({x: y for x, y in zip(lp.classes_, lp.predict_proba(sample())[0])})
+def predict_proba(input_path=None, model_path=None):
+    lp = get_model(model_path)
+    data_sample = sample() if input_path is None else get_external_sample(input_path)
+    print(
+        {x: y for x, y in
+         zip(lp.classes_, lp.predict_proba(data_sample)[0])})
 
 
-def predict():
-    lp = get_model()
-    return lp.predict(sample())[0]
+def predict(input_path=None, model_path=None):
+    lp = get_model(model_path)
+    data_sample = sample() if input_path is None else get_external_sample(input_path)
+    return lp.predict(data_sample)[0]
 
 
-def crossval(clf=None, X=None, y=None, folds=10, n=5):
+def crossval(clf=None, X=None, y=None, folds=10, n=5, path=None):
     if X is None or y is None:
-        X, y = get_train_data()
-    clf = clf or get_model()
+        X, y = get_train_data(path)
+    clf = clf or get_model(path)
     tot = 0
     print("KFold folds={}, running {} times".format(folds, n))
     for i in range(n):
@@ -31,8 +35,8 @@ def crossval(clf=None, X=None, y=None, folds=10, n=5):
     return tot / n
 
 
-def locations():
-    X, y = get_train_data()
+def locations(path=None):
+    X, y = get_train_data(path)
     if len(y) == 0:  # pragma: no cover
         msg = "No location samples available. First learn a location, e.g. with `whereami learn -l kitchen`."
         print(msg)
