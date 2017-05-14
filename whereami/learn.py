874ef7977bc41d7cf9f3873b8b3684b7d4c10965
Pascal van Kooten <kootenpv@gmail.com>
2017-05-14 18:26:17 +0200

    learning should wait between probes

--- whereami/learn.py
+++ whereami/learn.py
@@ -1,4 +1,6 @@
+import time
 import json
+
 from tqdm import tqdm
 
 from whereami.get_data import sample
@@ -14,10 +16,12 @@ def write_data(label_path, data):
         f.write("\n")
 
 
-def learn(label, n=100, device=""):
+def learn(label, n=20, device=""):
     path = ensure_whereami_path()
     label_path = get_label_file(path, label + ".txt")
-    for _ in tqdm(range(n)):
+    for i in tqdm(range(n)):
+        if i != 0:
+            time.sleep(15)
         try:
             new_sample = sample(device)
             if new_sample:
